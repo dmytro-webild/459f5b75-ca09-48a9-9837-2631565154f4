d161658c07b7c7cc13bfb0f7c7e4e263acca1cdb
bender <kudin.dima@gmail.com>
2026-06-07 23:50:28 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,6 +111,7 @@ export default function LandingPage() {
       imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3EpOjPEO4eL38LZ8Wh985rob2ul/uploaded-1780875154925-w091cu4a.jpg"
       imageAlt="Kathleen Reyes professional photo"
       mediaAnimation="slide-up"
+      imageClassName="object-contain"
     />
   </div>
 
